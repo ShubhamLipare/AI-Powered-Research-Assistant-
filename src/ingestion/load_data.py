02d65eeca708a116a0dce5d04e663b2d3e32e413
Shubham <shubhamlipare33@gmail.com>
2025-02-26 23:56:39 +0530

    adding folder strutures and load_data.py

---
+++ src/ingestion/load_data.py
@@ -0,0 +1,30 @@
+import os
+import PyPDF2
+import sys
+from src.logger import logging
+from src.exception import CustomException
+
+def load_pdf(path):
+    "Load text from PDF file."
+
+    try:
+        if not os.path.exists(path):
+            logging.info(f"File not found:{path}")
+            return 
+        logging.info("Loading data from PDF")
+        with open(path,"rb") as file:
+            reader=PyPDF2.PdfReader(file)
+            text="\n".join([page.extract_text() for page in reader.pages])
+
+        if not text.strip():
+            logging.info("Extracted text is empty. Check the PDF content.")
+            return None
+        
+        logging.info("Data loaded sucessfully!!!")
+        return text
+    
+    except Exception  as e:
+        raise CustomException(e,sys)
+    
+if __name__=="__main__":
+    text=load_pdf("G:\Resume projects\std4-bb-evs2.pdf")
